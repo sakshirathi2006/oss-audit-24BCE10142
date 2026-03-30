990e7f88deea957b90a19aa9f854acd6248dd02b
sakshirathi2006 <sakshi.24bce10142@vitbhopal.ac.in>
2026-03-31 04:47:05 +0530

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,11 +1,32 @@
-#!/bin/bash
-echo "===== Git Installation Check ====="
-if command -v git >/dev/null 2>&1
-then
-   echo "Git is installed on this system."
-   echo "Installed Git version:"
-   git --version
-else
-   echo "Git is Not installed on this system."
-fi
-echo "===check completed==="
+#!/bin/bash 
+echo "----- Log File Analyzer -----" 
+# Check if user provided both arguments 
+if [ $# -lt 2 ] 
+then 
+echo "Usage: $0 <file_path> <keyword>" 
+exit 1 
+f
+i 
+# Store input arguments 
+f
+ile=$1 
+keyword=$2 
+count=0 
+# Verify file exists before processing 
+if [ ! -f "$file" ] 
+then 
+echo "File not found." 
+exit 1 
+f
+i 
+# Read file line by line and search keyword 
+while read -r line 
+do 
+if echo "$line" | grep -i "$keyword" > /dev/null 
+then 
+count=$((count+1)) 
+f
+i 
+done < "$file" 
+# Print final count 
+echo "The keyword '$keyword' appeared $count times in the file."
